a5de01d86c58707844125f776371997f274c7e9d
pericabartulovic <pericab35@gmail.com>
2021-11-11 00:22:13 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 11;
+export const KEY = '37ad0266-c018-472a-871e-b49f2f199e11';
+export const MODAL_CLOSE_SEC = 2.5;
